42bc809888525b1c6e405153c519221355d79dfd
mevshnu <vishnusivan1000@gmail.com>
2022-11-29 16:54:23 +0530

    ioi

--- src/app/quotesapp/quotesapp.component.ts
+++ src/app/quotesapp/quotesapp.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-quotesapp',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./quotesapp.component.css']
 })
 export class QuotesappComponent {
-
+  constructor(private api:ApiService)
+  {
+    api.fetchquotes().subscribe(
+      (response)=>
+      {
+        this.data8=response;
+      }
+    )
+  }
+data8:any={}
 }
