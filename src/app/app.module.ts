ed9ff445aa93b0b91b7a2d9e5e5bda704d2edd3a
mevshnu <vishnusivan1000@gmail.com>
2022-11-29 09:19:40 +0530

    pol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,69 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { PassengerComponent } from './passenger/passenger.component';
+import { UsPublicComponent } from './us-public/us-public.component';
+import { PublicApiLinkComponent } from './public-api-link/public-api-link.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { UserdataComponent } from './userdata/userdata.component';
+import { TodoComponent } from './todo/todo.component';
+import { QuotesappComponent } from './quotesapp/quotesapp.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:PassengerComponent
+  },
+  {
+    path:"product",
+    component:ProductListComponent
+  },
+  {
+    path:"public",
+    component:PublicApiLinkComponent
+  },
+  {
+    path:"quotes",
+    component:QuotesappComponent
+  },
+  {
+    path:"todo",
+    component:TodoComponent
+  },
+  {
+    path:"us",
+    component:UsPublicComponent
+  },
+  {
+    path:"userdata",
+    component:UserdataComponent
+  },
+  {
+    path:"userinfo",
+    component:UserinfoComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    PassengerComponent,
+    UsPublicComponent,
+    PublicApiLinkComponent,
+    UserinfoComponent,
+    ProductListComponent,
+    UserdataComponent,
+    TodoComponent,
+    QuotesappComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/quotesapp/quotesapp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quotesapp',
+  templateUrl: './quotesapp.component.html',
+  styleUrls: ['./quotesapp.component.css']
+})
+export class QuotesappComponent {
+
+}
